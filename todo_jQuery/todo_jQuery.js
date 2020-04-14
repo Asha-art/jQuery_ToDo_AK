9fd4d30f6941a4729c7978fa575b10ae21e2654c
Asha-art <62064699+Asha-art@users.noreply.github.com>
2020-04-13 22:49:06 -0400

    add html css js files

--- todo_jQuery/todo_jQuery.js
+++ todo_jQuery/todo_jQuery.js
@@ -4,22 +4,28 @@
 $(document).ready(function() {
     $('button').on("click",function addList() {
         var text = $("#addTodo").val();   //getting value of text input element
-        var li = $('<li/>')               //adding list value to var li 
+        if(text === ''){
+        alert("add something to the list");
+        }else{
+         var li = $('<li/>')               //adding list value to var li 
           .text(text)                      //The text() method sets or returns the text content of the selected elements.      
-        
-    //    .on("click",function addList() { 
-    //     $('#list').css("text-decoration", "line-through"); //strikethrough
-    //    });
-   
-      $('ul').on("click",function addList() { 
-        
-            $('#list').remove()
-        });       // remove the item when clicked
-         
-       
+          $("#list").append(li); 
+        }
+     
+        $('#list').on("click",'li',function removeList(){  // when clicked strike through and delete
+        $(this).wrap("<strike>")
+        //$(this).css({"text-decoration": "overline"}); 
+        });    
 
-            $("#list").append(li);  //inserts the specified content as the last child or in the end of the list       
-        
+        $('#list').on("click",'li',function removeList(){ 
+        $(this).remove()  // remove the item when clicked
+        });
+
+        $('#addTodo').on('keypress',function(event){  //ENTER BUTTON
+          if(event.keyCode === 13){
+            $('#addBtn').click();
+          }
+        });
             $("input:text").val("");  //clear the input field
  });  
 });
